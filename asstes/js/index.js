66367d7b7d44a3d24dfd7f0b054827d00cf45f11
Caio <caiovilarouca@gmail.com>
2026-04-29 03:19:56 -0300

    feat (index.js service modal close)

--- asstes/js/index.js
+++ asstes/js/index.js
@@ -8,3 +8,26 @@ function scrollHeader() {
         header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+/* ======== SERVICES MODAL ======== */
+const modalViews = document.querySelectorAll('.services-modal'),
+    modalBtns = document.querySelectorAll('.services-button'),
+    modalClose = document.querySelectorAll('.services-modal-close')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () =>{
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+        })
+    })
+})
