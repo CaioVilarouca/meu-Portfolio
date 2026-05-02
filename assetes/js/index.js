1a29d9d1cd4987176491d640ee7b28172dd0d2ab
Caio <caiovilarouca@gmail.com>
2026-05-02 01:33:28 -0300

    feat (JS DARK LIGHT THEME)

--- assetes/js/index.js
+++ assetes/js/index.js
@@ -52,4 +52,26 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/* ======== SCROLL SECTIONS ACTIVE LINK ======== */
+/* ======== DARK LIGHT THEME ======== */
+const themeButton = document.getElementById('theme-button')
+const lightTheme = 'light-theme'
+const iconTheme = 'ri-sun-line'
+
+const selectedTheme = localStorage.getItem('selected-Theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(lightTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](lightTheme)
+    themeButton.classList[selectedIcon === 'ri-moon-line'  ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(lightTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
